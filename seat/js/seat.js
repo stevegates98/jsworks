ea009348ea8ede319ca66d7204331ce23936c700
stevegates98 <stevegates98@naver.com>
2022-05-24 15:41:35 +0900

    sav

--- seat/js/seat.js
+++ seat/js/seat.js
@@ -12,12 +12,14 @@ else {
     rowNum = parseInt(customNum / colNum) + 1;
 }
 
-// document.write(rowNum)
+document.write("<table>");
 for(let i = 0; i < rowNum; i++){
+    document.write("<tr>");
     for(let j = 1; j <= colNum; j++){
         let seatNum = i * colNum + j;
         if(seatNum > customNum) break;
-        document.write("좌석" + seatNum + " ");
+        document.write("<td>좌석" + seatNum + "</td>");
     }
-    document.write("<br>");
+    document.write("</tr>");
 }
+document.write("</table>");
