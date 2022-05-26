31512283d100da5ba3b4b9fca2e1d3bf01cadaec
stevegates98 <stevegates98@naver.com>
2022-05-26 12:11:46 +0900

    js

---
+++ day14/js/cart.js
@@ -0,0 +1,24 @@
+// 장바구니 배열 사용하기
+let cart = new Array(5);
+// console.log(cart.length + "개");
+// 입력
+cart = ["생수", "라면", "커피", "계란", "쌀"];
+
+// let cart = new Array("생수", "라면", "커피", "계란");
+
+// "커피"를 출력
+document.write(cart[2] + "<br><br>");
+
+// "계란"을 "사과"로 변경하세요.
+cart[3] = "사과";
+
+// "라면"을 삭제하세요.
+delete cart[1];
+
+// 목록 전체 출력
+for (let i = 0; i < cart.length; i++) {
+    document.write(cart[i] + " ");
+}
+
+// 객체(object) 출력
+console.log(cart);
